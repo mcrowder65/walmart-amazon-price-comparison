ae84a2ac4d65062e531b8b75df80f2969528861a
Matt Crowder <mcrowder65@gmail.com>
2018-08-20 21:20:16 -0400

    move server over, unfortunately this doesn't work yet.

--- src/server/index.js
+++ src/server/index.js
@@ -7,43 +7,43 @@ const HOST = "localhost";
 const PORT = 3000;
 
 async function StartServer() {
-  const server = new Hapi.server({
-    host: HOST,
-    port: PORT
-  });
-
-  await server.register({
-    plugin: graphqlHapi,
-    options: {
-      path: "/graphql",
-      graphqlOptions: {
-        schema
-      },
-      route: {
-        cors: true
-      }
-    }
-  });
-  await server.register({
-    plugin: graphiqlHapi,
-    options: {
-      path: "/graphiql",
-      graphiqlOptions: {
-        schema,
-        endpointURL: "/graphql"
-      },
-      route: {
-        cors: true
-      }
-    }
-  });
   try {
-    await server.start();
-  } catch (err) {
-    console.log(`Error while starting server: ${err.message}`);
-  }
+    const server = new Hapi.server({
+      host: HOST,
+      port: PORT
+    });
 
-  console.log(`Server running at: ${server.info.uri}`);
+    await server.register({
+      plugin: graphqlHapi,
+      options: {
+        path: "/graphql",
+        graphqlOptions: {
+          schema
+        },
+        route: {
+          cors: true
+        }
+      }
+    });
+    await server.register({
+      plugin: graphiqlHapi,
+      options: {
+        path: "/graphiql",
+        graphiqlOptions: {
+          schema,
+          endpointURL: "/graphql"
+        },
+        route: {
+          cors: true
+        }
+      }
+    });
+    await server.start();
+
+    console.log(`Server running at: ${server.info.uri}`);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 StartServer();
