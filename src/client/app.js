3e71601705e7f37f5c158b6c61f8d17305c1ec75
Matt Crowder <mcrowder65@gmail.com>
2018-08-20 21:23:29 -0400

    oops added client.

--- src/client/app.js
+++ src/client/app.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
 
-import registerServiceWorker from "../registerServiceWorker";
 import Router from "./router";
+import apolloClient from "./graphql/client";
 
+/* global window */
 if (process.env.NODE_ENV === "development" && module.hot) {
   module.hot.accept();
 }
-const App = () => <Router />;
+
+const App = () => (
+  <ApolloProvider client={apolloClient}>
+    <Router />
+  </ApolloProvider>
+);
 
 /* global document */
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
